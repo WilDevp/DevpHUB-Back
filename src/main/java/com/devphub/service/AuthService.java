b8f8c5e3285de2a53e947cd547ee4152b03ff07e
WilDevp <wilmar.garciava@gmail.com>
2022-05-16 12:30:15 -0500

    generate token

--- src/main/java/com/devphub/service/AuthService.java
+++ src/main/java/com/devphub/service/AuthService.java
@@ -2,17 +2,26 @@ package com.devphub.service;
 
 import com.devphub.dto.RegisterRequest;
 import com.devphub.model.User;
+import com.devphub.model.VerificationToken;
+import com.devphub.repository.UserRepository;
+import com.devphub.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class AuthService {
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+
     public void signup(RegisterRequest registerRequest){
         User user = new User();
         user.setUsername(registerRequest.getUsername());
@@ -20,5 +29,19 @@ public class AuthService {
         user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setCreated(Instant.now());
         user.setEnabled(false);
+
+        userRepository.save(user);
+
+        String token = generateVerifcationToken(user);
+    }
+
+    private String generateVerifcationToken(User user){
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
     }
 }
